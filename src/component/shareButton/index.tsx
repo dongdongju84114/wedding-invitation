4b7879c6a5441df8e2161988ea525fd6b0f5ff58
dongdongju84114 <redking011444@gmail.com>
2026-05-06 17:00:57 +0900

    improve map

--- src/component/shareButton/index.tsx
+++ src/component/shareButton/index.tsx
@@ -2,17 +2,13 @@ import {
   BRIDE_FULLNAME,
   GROOM_FULLNAME,
   LOCATION,
-  SHARE_ADDRESS,
-  SHARE_ADDRESS_TITLE,
+  LOCATION_ADDRESS,
   WEDDING_DATE,
   WEDDING_DATE_FORMAT,
 } from "../../const"
 import ktalkIcon from "../../icons/ktalk-icon.png"
 import { LazyDiv } from "../lazyDiv"
 import { useKakao } from "../store"
-
-const baseUrl = import.meta.env.BASE_URL
-const shareUrl = new URL(import.meta.env.BASE_URL, window.location.origin).href
 
 /**
  * 카카오톡으로 초대장을 공유할 수 있는 버튼 컴포넌트입니다.
@@ -21,31 +17,35 @@ const shareUrl = new URL(import.meta.env.BASE_URL, window.location.origin).href
  */
 export const ShareButton = () => {
   const kakao = useKakao()
+
   return (
     <LazyDiv className="footer share-button">
       <button
         className="ktalk-share"
         onClick={() => {
-          // 카카오 SDK 로드 전이면 무시
           if (!kakao) {
             return
           }
 
-          // 카카오톡 공유 전송 (위치 기반 템플릿 사용)
+          const shareUrl = new URL(
+            import.meta.env.BASE_URL,
+            window.location.origin,
+          ).href
+
+          const mapPageUrl = new URL("map", shareUrl).href
+          const imageUrl = new URL("preview_image.png", shareUrl).href
+
           kakao.Share.sendDefault({
-            objectType: "location",
-            address: SHARE_ADDRESS,
-            addressTitle: SHARE_ADDRESS_TITLE,
+            objectType: "feed",
             content: {
               title: `${GROOM_FULLNAME} ❤️ ${BRIDE_FULLNAME}의 결혼식에 초대합니다.`,
               description:
-                WEDDING_DATE.format(WEDDING_DATE_FORMAT) + "\n" + LOCATION,
-              imageUrl:
-                window.location.protocol +
-                "//" +
-                window.location.host +
-                baseUrl +
-                "/preview_image.png",
+                WEDDING_DATE.format(WEDDING_DATE_FORMAT) +
+                "\n" +
+                LOCATION +
+                "\n" +
+                LOCATION_ADDRESS,
+              imageUrl,
               link: {
                 mobileWebUrl: shareUrl,
                 webUrl: shareUrl,
@@ -57,6 +57,13 @@ export const ShareButton = () => {
                 link: {
                   mobileWebUrl: shareUrl,
                   webUrl: shareUrl,
+                },
+              },
+              {
+                title: "오시는 길",
+                link: {
+                  mobileWebUrl: mapPageUrl,
+                  webUrl: mapPageUrl,
                 },
               },
             ],
