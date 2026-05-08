bb81db744f36582e6b1701b421ab3c4f8d64f61f
dongdongju84114 <redking011444@gmail.com>
2026-05-08 23:41:50 +0900

    improve map

--- src/images/index.ts
+++ src/images/index.ts
@@ -1,16 +1,13 @@
 import coverImage from "./cover.jpeg"
-import image1 from "./image1.png"
-import image2 from "./image2.png"
-import image3 from "./image3.png"
-import image4 from "./image4.png"
-import image5 from "./image5.png"
-import image6 from "./image6.png"
-import image7 from "./image7.png"
-import image8 from "./image8.png"
-import image9 from "./image9.png"
-import image10 from "./image10.png"
-import image11 from "./image11.png"
-import image12 from "./image12.png"
+import image1 from "./image1.jpeg"
+import image2 from "./image2.jpeg"
+import image3 from "./image3.jpeg"
+import image4 from "./image4.jpeg"
+import image5 from "./image5.jpeg"
+import image6 from "./image6.jpeg"
+import image7 from "./image7.jpeg"
+import image8 from "./image8.jpeg"
+import image9 from "./image9.jpeg"
 
 /**
  * 메인 커버 이미지
@@ -30,7 +27,4 @@ export const GALLERY_IMAGES = [
   image7,
   image8,
   image9,
-  image10,
-  image11,
-  image12,
 ]
