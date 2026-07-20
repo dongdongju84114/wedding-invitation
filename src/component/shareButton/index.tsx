8034a5cb25703a04aa88dbac76e037effe795cd4
dongdongju84114 <redking011444@gmail.com>
2026-07-20 17:41:38 +0900

    Fix Kakao share source link

--- src/component/shareButton/index.tsx
+++ src/component/shareButton/index.tsx
@@ -1,11 +1,3 @@
-import {
-  BRIDE_FULLNAME,
-  GROOM_FULLNAME,
-  LOCATION,
-  LOCATION_ADDRESS,
-  WEDDING_DATE,
-  WEDDING_DATE_FORMAT,
-} from "../../const"
 import ktalkIcon from "../../icons/ktalk-icon.png"
 import { LazyDiv } from "../lazyDiv"
 import { useKakao } from "../store"
@@ -27,46 +19,18 @@ export const ShareButton = () => {
             return
           }
 
-          const shareUrl = 'https://dongdongju84114.github.io/wedding-invitation/';
-
-          const mapPageUrl = 'https://place.map.kakao.com/767048338';
+          const shareUrl =
+            "https://dongdongju84114.github.io/wedding-invitation/"
           const imageUrl = new URL(
             "kakao_preview.png?v=20260720-square",
             shareUrl,
           ).href
 
-          kakao.Share.sendDefault({
-            objectType: "feed",
-            content: {
-              title: `${GROOM_FULLNAME} ❤️ ${BRIDE_FULLNAME}의 결혼식에 초대합니다.`,
-              description:
-                WEDDING_DATE.format(WEDDING_DATE_FORMAT) +
-                "\n" +
-                LOCATION +
-                "\n" +
-                LOCATION_ADDRESS,
-              imageUrl,
-              link: {
-                mobileWebUrl: 'https://dongdongju84114.github.io/wedding-invitation/',
-                webUrl: 'https://dongdongju84114.github.io/wedding-invitation/',
-              },
+          kakao.Share.sendCustom({
+            templateId: 135481,
+            templateArgs: {
+              IMAGE_URL: imageUrl,
             },
-            buttons: [
-              {
-                title: "초대장 보기",
-                link: {
-                  mobileWebUrl: 'https://dongdongju84114.github.io/wedding-invitation/',
-                  webUrl: 'https://dongdongju84114.github.io/wedding-invitation/',
-                },
-              },
-              {
-                title: "오시는 길",
-                link: {
-                  mobileWebUrl: mapPageUrl,
-                  webUrl: mapPageUrl,
-                },
-              },
-            ],
           })
         }}
       >
